8c23c601d0b0ccd16498cb4ade6f4242e01083b9
cema-sp <cema.rus@gmail.com>
2015-07-10 17:50:22 +0300

    Adding buttons selectors

--- features/step_definitions/content_steps.rb
+++ features/step_definitions/content_steps.rb
@@ -1,5 +1,17 @@
-# require 'pry'
-
 То(/^я вижу текст "([^"]*)"$/) do |text|
   expect(@response.body).to have_content text
+end
+
+То(/^я вижу текст:$/) do |text_data|
+  text_data.hashes.each do |text_row|
+    expect(@response.body).to have_content text_row[:text]
+  end
+end
+
+То(/^я вижу кнопку "([^"]*)"$/) do |text|
+  expect(@response.body).to have_selector(:link_or_button, text)
+end
+
+То(/^я вижу (\d+) кнопки "([^"]*)"$/) do |number, text|
+  expect(@response.body).to have_selector(:link_or_button, text, count: number)
 end
